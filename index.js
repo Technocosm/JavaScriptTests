272f5cc1d1d2640ed12d0aa04e087e4761ac3977
RYAN DUNN <c0001592@hallam.shu.ac.uk>
2025-04-26 18:11:15 +0100

    First Attempts to Retrieve submission data and cut it down to 100 entries for validation.

--- index.js
+++ index.js
@@ -13,4 +13,29 @@ async function sortHackerNewsArticles() {
 
 (async () => {
   await sortHackerNewsArticles();
+
+  //Begin Validation:
+
+  //Get all submissions
+  const json =
+  fetch("https://news.ycombinator.com/newest")              //Fetch from URL
+  .then(response =>                                         //(We don't have the html)
+    {
+      if(response.ok)                                       //Error Check
+      {
+        return response.json();                             //Return Result on Success
+      }
+      else
+      {
+        throw new Error('Response not ok');                 //Failstate
+      }
+    })
+  .catch(error => console.error("Error fetching!", error)); //Log Error
+  
+  //Extract Submissions from Json
+  const submissions = json.getElementsByClassName("athing submission");
+
+  //We were only asked to validate the first 100, so filter those out.
+  const validateSize = 100;
+  submissions = submissions.slice(0, validateSize);
 })();
